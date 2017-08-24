eff0f23d34ee4ea4ee0a1971a17600570ded30b0
ashishrp <a4ashishrp@gmail.com>
2017-08-24 13:08:21 +0530

    async sorting

--- ContactApp_ajax_React/src/contact/index.js
+++ ContactApp_ajax_React/src/contact/index.js
@@ -1,4 +1,6 @@
-import { Component } from 'react';
+import {
+  Component
+} from 'react';
 import contacts from './contact';
 import axios from 'axios'
 
@@ -8,11 +10,15 @@ class contact extends Component {
     super(props);
     this.state = {
       contacts: [],
-      _edit: {}
+      _edit: {},
+      array: ["2", "3223342", "232", "32423", "22"]
+      // array: ["ashisasdasdas435435h", "4353454sfsonemoreguy", "somemore23423423", "asdas324344512", "afsdf767^&*^*sfada", "afsdf767^&*^*sfadasa"]
     };
     this.getData = this.getData.bind(this);
     this.delete = this.delete.bind(this);
     this.edit = this.edit.bind(this);
+    this.compareData = this.compareData.bind(this);
+
   }
   delete(event) {
     console.log('delete')
@@ -30,20 +36,71 @@ class contact extends Component {
   edit(event) {
     console.log('edit')
     let index = event.currentTarget.dataset.id
-    this.setState({ index: index })
-    this.setState({ _edit: this.state.contacts[index] })
-    this.setState({ showModal: true })
+    this.setState({
+      index: index
+    })
+    this.setState({
+      _edit: this.state.contacts[index]
+    })
+    this.setState({
+      showModal: true
+    })
   }
   componentWillMount() {
     console.log('componentWillMount')
     this.getData();
+    console.log(this.state.array);
+    (async function loop(array) {
+      for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+          let first = array[i];
+          let sec = array[j];
+          await axios.post(`/app/compare`, {
+              first,
+              sec,
+              type: ""
+            })
+            .then(res => {
+              console.log('data', res.data.data)
+              if (res.data.data > 0) {
+                console.log("-1");
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+              }
+
+            }).catch(err => {
+              console.log(err)
+            });
+
+        }
+      }
+      console.log(array);
+    })(this.state.array);
 
   }
+
+
   getData() {
     axios.get(`/app/contacts`)
       .then(res => {
         console.log('data', res.data.data)
-        this.setState({ contacts: res.data.data })
+        this.setState({
+          contacts: res.data.data
+        })
+      }).catch(err => {
+        console.log(err)
+      });
+  }
+  compareData(first, sec) {
+    axios.post(`/app/compare`, {
+        first,
+        sec,
+        type: ""
+      })
+      .then(res => {
+        console.log('data', res.data.data)
+        return res.data.data;
       }).catch(err => {
         console.log(err)
       });
